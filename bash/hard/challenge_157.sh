aef2081fc40f904e44e544d141e7b41aa3cdafd4
Topher Donovan <christopher.schalk@gmail.com>
2016-03-29 23:26:23 -0600

    Bash 239, optimized (slightly) 157

--- bash/hard/challenge_157.sh
+++ bash/hard/challenge_157.sh
@@ -12,9 +12,9 @@ function readMaze {
     done < ${1}
   
     LENGTH=${#MAZE}
-    HEIGHT=$(( LENGTH / WIDTH)) # | bc)
-    START=$(echo "${MAZE}" | grep -aob ' ' | awk -F: '{print $1}' | head -n 1)
-    END=$(echo "${MAZE}" | grep -aob ' ' | awk -F: '{print $1}' | tail -n 1)
+    let "HEIGHT = LENGTH / WIDTH"
+    START=`echo "${MAZE}" | grep -aob ' ' | awk -F: '{print $1}' | head -n 1`
+    END=`echo "${MAZE}" | grep -aob ' ' | awk -F: '{print $1}' | tail -n 1`
 }
 
 function solve {
@@ -33,46 +33,48 @@ function solve {
 function divine {
     coord=${1}
 
-    nextx=$((coord + 1))
+    let "nextx = coord + 1"
     update ${nextx} ${coord}
 
-    prevx=$((coord - 1))
+    let "prevx = coord - 1"
     update ${prevx} ${coord}
 
-    nexty=$((coord + WIDTH))
+    let "nexty = coord + WIDTH"
     update ${nexty} ${coord}
     
-    prevy=$((coord - WIDTH))
+    let "prevy = coord - WIDTH"
     update ${prevy} ${coord}
 }
 
 function getScore {
     scoreCoord=${1}
     scoreString=${cells[${scoreCoord}]}
-    echo ${scoreString} | /usr/bin/awk -F':' '{ print $1 }'
+    RETVAL=`echo ${scoreString} | /usr/bin/awk -F':' '{ print $1 }'`
 }
 
 function getCoordinate {
     coordCoord=${1}
     coordString=${cells[${coordCoord}]}
-    echo "${coordString}" | /usr/bin/awk -F: '{print $2}'
+    RETVAL=`echo "${coordString}" | /usr/bin/awk -F: '{print $2}'`
 }
 
 function update {
     nextCoord=${1}
     currentCoord=${2}
-    currentScore=$(getScore ${currentCoord})
+    getScore ${currentCoord}
+    currentScore=${RETVAL}
  
     if [[ ${nextCoord} -gt 0 ]] && [[ ${nextCoord} -lt ${#MAZE} ]]; then
         char=${MAZE:${nextCoord}:1}
         if [[ "${char}" == " " ]]; then
             nextCell=${cells[${nextCoord}]}
-            nextScore=$((currentScore + 1))
+	    let "nextScore = currentScore + 1"
             if [[ -z ${nextCell} ]]; then
                 cells[${nextCoord}]="${nextScore}:${currentCoord}"
                 PATH+=(${nextCoord})
             else
-                oldScore=$(getScore ${nextCoord})
+		getScore ${nextCoord}
+		oldScore=${RETVAL}
                 if [[ ${nextScore} -lt ${oldScore} ]]; then
                     cells[${nextCoord}]="${nextScore}:${currentCoord}"
                 fi
@@ -84,13 +86,15 @@ function update {
 function writeAnswer {
     writeCoord=${END}
     while [[ ${writeCoord} -gt ${START} ]]; do
-        MAZE="${MAZE:0:${writeCoord}}+${MAZE:$((writeCoord+1))}"
-        writeCoord=$(getCoordinate ${writeCoord})
+	let "nextCoord = writeCoord + 1"
+        MAZE="${MAZE:0:${writeCoord}}+${MAZE:${nextCoord}}"
+	getCoordinate ${writeCoord}
+	writeCoord=${RETVAL}
     done
 }
 
 function printMaze {
-    for row in $(/usr/bin/seq 0 ${HEIGHT}); do
+    for row in `/usr/bin/seq 0 ${HEIGHT}`; do
         echo "${MAZE:((row * WIDTH)):${WIDTH}}"
     done 
 }
