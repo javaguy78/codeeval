83b2023d44b338251db2e781b4b98fbe87355f71
Topher Donovan <christopher.schalk@gmail.com>
2016-03-30 16:29:57 -0600

    moderate challenge 16: Number of ones

---
+++ cpp/moderate/challenge_16.cpp
@@ -0,0 +1,27 @@
+// Created by cschalk on 3/30/16.
+// Challenge 16 : Number of ones
+#include <iostream>
+#include <fstream>
+#include <bitset>
+
+int getBit(int val, int bit) {
+    return (val & (1 << bit)) >> bit;
+}
+
+int main(int argc, char ** argv) {
+    std::ifstream input {argv[1]};
+    int test;
+
+    while (input >> test) {
+        std::string binary = std::bitset<64>(test).to_string();
+        int count = 0;
+        for (char c : binary) {
+            if (c == '1') {
+                count++;
+            }
+        }
+        std::cout << count << std::endl;
+    }
+
+    return 0;
+}
